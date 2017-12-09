0f33c9daa4c4193adf7d15bd110f511bf6cf5a5c
Yu Ishihara <yuishihara1225@gmail.com>
2017-12-09 15:48:20 +0900

    Add change typeface for texts

---
+++ app/src/main/java/jp/hackday10th/yay/hackday/HackDayApplication.java
@@ -0,0 +1,10 @@
+package jp.hackday10th.yay.hackday;
+
+import android.app.Application;
+
+public class HackDayApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/jp/hackday10th/yay/hackday/fragments/ScaleFragment.java
+++ app/src/main/java/jp/hackday10th/yay/hackday/fragments/ScaleFragment.java
@@ -1,6 +1,7 @@
 package jp.hackday10th.yay.hackday.fragments;
 
 import android.databinding.DataBindingUtil;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -17,6 +18,7 @@ import jp.hackday10th.yay.hackday.views.TouchHandleView;
 
 public class ScaleFragment extends Fragment {
     private static final String TAG = ScaleFragment.class.getSimpleName();
+    private static final String TYPE_FACE_PATH = "fonts/DINNextLTPro-Regular.otf";
     private FragmentScaleBinding mBinding;
     private WeightCalculator mWeightCalculator = new WeightCalculator();
 
@@ -24,6 +26,8 @@ public class ScaleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_scale, null, false);
+        Typeface type = Typeface.createFromAsset(getContext().getAssets(), TYPE_FACE_PATH);
+        mBinding.weight.setTypeface(type);
         return mBinding.getRoot();
     }
 
