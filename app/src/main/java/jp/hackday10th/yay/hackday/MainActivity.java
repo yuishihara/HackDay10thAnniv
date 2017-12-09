deaa18ccf173e4d30c5dad9c6ac0925547094e49
Yu Ishihara <yuishihara1225@gmail.com>
2017-12-09 14:40:37 +0900

    Add set activity always full screen

--- app/src/main/java/jp/hackday10th/yay/hackday/MainActivity.java
+++ app/src/main/java/jp/hackday10th/yay/hackday/MainActivity.java
@@ -3,15 +3,31 @@ package jp.hackday10th.yay.hackday;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import jp.hackday10th.yay.hackday.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int UI_VISIBILITY_FLAGS = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
     private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getWindow().getDecorView().setSystemUiVisibility(UI_VISIBILITY_FLAGS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
